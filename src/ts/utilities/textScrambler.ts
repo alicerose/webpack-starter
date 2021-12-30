802370ca344278a2814f4f7c579f450fd5acf4b7
Takehiro Kato <czk@seeds-9.com>
2021-12-31 01:53:36 +0900

    modify text scrambler, expand frames refer to text length

--- src/ts/utilities/textScrambler.ts
+++ src/ts/utilities/textScrambler.ts
@@ -22,7 +22,7 @@ class TextScramblerClass {
   element: Element;
   source: string[];
   result: string[];
-  loop: number;
+  loop = 0;
   // スクランブルを書ける時間（ms）
   frames = 30;
   // 動作FPS
@@ -34,18 +34,24 @@ class TextScramblerClass {
     this.element = element;
     this.source = element.textContent ? element.textContent.split('') : [];
     this.result = [];
-    this.loop = 0;
+    this.frames += this.source.length;
   }
 
+  /**
+   * 実行関数
+   */
   public run() {
     const arr: string[] = [];
     const pl = this.pool.split('');
     let flag = false;
     this.source.forEach((src: string, i: number) => {
+      // ループ終了から逆算した文字列位置ならスクランブル終了フラグを建てる
       flag = this.source.length - i > this.frames - this.loop;
-      if(flag || this.loop === this.frames - 1) {
+      if (flag || this.loop === this.frames - 1) {
+        // 素の文字を配列に戻す
         arr.push(src);
       } else {
+        // スクランブルした文字列を配列に戻す
         arr.push(pl[Math.floor(Math.random() * pl.length)]);
       }
     });
@@ -53,14 +59,20 @@ class TextScramblerClass {
     this.reflect();
   }
 
+  /**
+   * 描画関数
+   * ループ値をインクリメントし、規定ループ数以下なら再実行する
+   */
   reflect() {
     // console.log('#' + this.loop, 'reflect', this.source, '=>', this.result.join(''));
     this.element.textContent = this.result.join('');
     this.loop++;
-    if(this.loop < this.frames) {
+    if (this.loop < this.frames) {
       setTimeout(() => {
         this.run();
       }, 1000 / this.fps);
+    } else {
+      console.log(this);
     }
   }
 }
