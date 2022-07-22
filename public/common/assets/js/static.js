443d98af47bfcfc2a5a8afa9bb3268a8cff1bf4c
Takehiro Kato <czk@seeds-9.com>
2022-07-22 16:59:11 +0900

    add static asset loader

---
+++ public/common/assets/js/static.js
@@ -0,0 +1,5 @@
+console.warn('static js loaded!');
+
+const sample = () => {
+  console.warn('[sample] get h1 with jquery:', $('h1').text());
+};
--- src/ts/utilities/index.ts
+++ src/ts/utilities/index.ts
@@ -4,22 +4,55 @@ import { UserAgent } from './userAgent';
 import { EnableJQuery } from '../vendors/EnableJQuery';
 import { ScrollDetector } from './scrollDetector';
 
+enum AssetType {
+  CSS = 'css',
+  JS = 'js'
+}
+
 export const Utilities = {
   init() {
-    this.global();
-
     const body: HTMLBodyElement = document.getElementsByTagName('body')[0];
     if (body.dataset.page) this.individual(body.dataset.page);
   },
   /**
-   * 全ページで使うユーティリティ
+   * 共通ユーティリティ関数郡
    */
   global() {
-    AnchorLink.init();
     EnableJQuery.init();
+  },
+  /**
+   * 共通ユーティリティ関数郡（DOM構築後）
+   */
+  domLoaded() {
+    this.loadStaticAsset(AssetType.JS, '/assets/js/static.js');
+    this.loadStaticAsset(AssetType.CSS, '/assets/css/static.css');
+  },
+  /**
+   * 共通ユーティリティ関数郡（読み込み完了後）
+   */
+  onLoad() {
+    AnchorLink.init();
     ScrollDetector.init();
     UserAgent.init();
     Viewport.init();
+  },
+  /**
+   * DOM構築時に静的ファイルを読み込む
+   * @param type AssetType ファイル形式
+   * @param path string ファイルパス
+   */
+  loadStaticAsset(type: AssetType, path: string) {
+    console.log('[Utilities] append static file:', path);
+    const dom = document.createElement(type === 'js' ? 'script' : 'link');
+    if('src' in dom) {
+      dom.src = path;
+    } else {
+      dom.rel = 'stylesheet';
+      dom.type = 'text/css';
+      dom.href = path;
+    }
+
+    document.getElementsByTagName('head')[0].appendChild(dom);
   },
   /**
    * 個別ページ専用のスクリプトを記述
