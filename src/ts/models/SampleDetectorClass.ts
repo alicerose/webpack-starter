07f638402b07c7b6370f5bca0dc791e0825f7a6d
Takehiro Kato <czk@seeds-9.com>
2022-07-24 02:51:03 +0900

    create sample extended class, add usage

---
+++ src/ts/models/SampleDetectorClass.ts
@@ -0,0 +1,35 @@
+import ScrollDetectClass from './ScrollDetectClass';
+
+/**
+ * @memo
+ * IntersectionObserverのラッパークラスの継承サンプル
+ * 最低限交差時・非交差時の挙動だけ実装すれば良い
+ * この2つの関数はabstractで定義しているので継承先で実装しないとエラーを返す
+ */
+export default class SampleDetectorClass extends ScrollDetectClass {
+
+  /**
+   * 交差時の挙動
+   * @param entry
+   */
+  isIntersecting(entry: IntersectionObserverEntry) {
+    const status = entry.isIntersecting ? 'true' : 'false';
+    entry.target.textContent = 'is intersecting:' + status;
+    entry.target.classList.add('active');
+
+    console.log(entry.target.id, entry.isIntersecting);
+  }
+
+  /**
+   * 非交差時の挙動
+   * @param entry
+   */
+  isNotIntersecting(entry: IntersectionObserverEntry) {
+    const status = entry.isIntersecting ? 'true' : 'false';
+    entry.target.textContent = 'is intersecting:' + status;
+    entry.target.classList.remove('active');
+
+    console.log(entry.target.id, entry.isIntersecting);
+  }
+
+}
--- src/ts/utilities/scrollDetector.ts
+++ src/ts/utilities/scrollDetector.ts
@@ -1,38 +1,16 @@
+import SampleDetectorClass from '../models/SampleDetectorClass';
+
 export const ScrollDetector = {
-  // スクロール時の監視設定
-  config: {
-    root      : null,
-    rootMargin: '0% 0px',
-    threshold : 0.5
-  },
-  // 監視対象要素
-  selector: 'main > *:not(.test-static)',
-  /**
-   * @todo 汎用性に乏しいのでmodel化してextendさせる
-   */
   init() {
-    const observer = new IntersectionObserver(this.callback, this.config);
 
-    const targets = document.querySelectorAll(this.selector);
-    targets.forEach(target => {
-      observer.observe(target);
-    });
-  },
-  /**
-   * 交差時のコールバック
-   * @param entries
-   */
-  callback(entries: IntersectionObserverEntry[]) {
-    entries.forEach(entry => {
-      console.log(entry);
-      const status = entry.isIntersecting ? 'true' : 'false';
-      entry.target.textContent = 'is intersecting:' + status;
-      if(entry.isIntersecting) {
-        entry.target.classList.add('active');
-      } else {
-        entry.target.classList.remove('active');
+    const sample = new SampleDetectorClass(
+      'main > *:not(.test-static)',
+      {
+        rootMargin: '0% 0px',
+        threshold : 0.5
       }
+    );
 
-    });
-  }
+    console.log(sample);
+  },
 };
