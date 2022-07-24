da6652b53753b3d48b585d8207ab55155813effc
Takehiro Kato <czk@seeds-9.com>
2022-07-24 22:53:34 +0900

    delete backup file

--- src/ts/utilities/scrollDetector.ts
+++ src/ts/utilities/scrollDetector.ts
@@ -3,14 +3,12 @@ import SampleDetectorClass from '../models/SampleDetectorClass';
 export const ScrollDetector = {
   init() {
 
-    const sample = new SampleDetectorClass(
+    console.log(new SampleDetectorClass(
       'main > *:not(.test-static)',
       {
         rootMargin: '0% 0px',
         threshold : 0.5
       }
-    );
-
-    console.log(sample);
+    ));
   },
 };
